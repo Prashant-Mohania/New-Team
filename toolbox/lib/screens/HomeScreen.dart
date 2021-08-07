f5e95ad4bc285d5b0884dde3020f98e943147ef3
Prashant Mohania <pramo.prem.7102@gmail.com>
2021-08-07 16:00:41 +0530

    Custom Buttom

--- toolbox/lib/screens/HomeScreen.dart
+++ toolbox/lib/screens/HomeScreen.dart
@@ -15,29 +15,65 @@ class HomeScreen extends StatelessWidget {
         padding: EdgeInsets.all(20),
         child: Column(
           children: [
-            Container(
-              padding: EdgeInsets.all(20),
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(20),
-                // color: MyConstants.myColor1,
-                gradient: LinearGradient(
-                  colors: [
-                    MyConstants.myColor1,
-                    MyConstants.backgroundColor,
-                  ],
+            InkWell(
+              onTap: () {
+                print("Instagram");
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => Faltu(),
+                  ),
+                );
+              },
+              child: Container(
+                padding: EdgeInsets.all(20),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  // color: MyConstants.myColor1,
+                  gradient: LinearGradient(
+                    colors: [
+                      MyConstants.myColor1,
+                      MyConstants.backgroundColor,
+                    ],
+                  ),
                 ),
-              ),
-              width: MediaQuery.of(context).size.width,
-              child: Text(
-                "Fake Instagram",
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 22,
-                  fontWeight: FontWeight.bold,
+                width: MediaQuery.of(context).size.width,
+                child: Text(
+                  "Fake Instagram",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 22,
+                    fontWeight: FontWeight.bold,
+                  ),
                 ),
               ),
             ),
           ],
+        ),
+      ),
+    );
+  }
+}
+
+class Faltu extends StatelessWidget {
+  const Faltu({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Faltu"),
+      ),
+      body: Container(
+        padding: EdgeInsets.all(20),
+        color: Colors.blue,
+        child: Center(
+          child: Container(
+            color: Colors.amber,
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height,
+            child: Text("Hello World"),
+          ),
         ),
       ),
     );
